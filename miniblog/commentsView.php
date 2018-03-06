6cd85a31a8b7458fe6c217f8f8dd30e15cd95847
Guillaume CARON <gc3d.42@gmail.com>
2018-03-06 14:57:43 +0100

    template pour comments OK

--- miniblog/commentsView.php
+++ miniblog/commentsView.php
@@ -1,59 +1,54 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Super Blog</title>
-	<link rel="stylesheet" type="text/css" href="blog.css">
+<?php $title = 'Super Blog'; ?>
 
-</head>
-<body>
-	<h1>Mon super blog !</h1>
-	<p><a href="index.php">Retour vers les news</a></p>
+<?php ob_start(); ?>
+<h1>Mon super blog !</h1>
+<p><a href="index.php">Retour vers les news</a></p>
 
 
 <!-- DISPLAY SELECTED POST -->
-	<div class="news">
-        <h3>
-            <?= htmlspecialchars($post['title']); ?>
-            <i>&nbsp;&nbsp;&nbsp;
-                le <?= $post['creation_date_fr']; ?>
-            </i>
-        </h3>
-		<p>
-			<?= htmlspecialchars($post['content']); ?>
-		</p>
-	</div>
+<div class="news">
+    <h3>
+        <?= htmlspecialchars($post['title']); ?>
+        <i>&nbsp;&nbsp;&nbsp;
+            le <?= $post['creation_date_fr']; ?>
+        </i>
+    </h3>
+    <p>
+        <?= htmlspecialchars($post['content']); ?>
+    </p>
+</div>
 	
 	
 
 
 <!-- DISPLAY COMMENTS RELATED WITH SELECTED POST -->
-	<h2>Commentaires</h2>
+<h2>Commentaires</h2>
 
-	<?php
-	while ($data = $comments->fetch())
-	{
-	?>
+<?php
+while ($data = $comments->fetch())
+{
+?>
 
-		<div class="commentaires">
-			<p>
-				<span class="who">
-                    <i>
-                        <strong>
-                            <?= htmlspecialchars($data['author']); ?>
-                        </strong>
-                        <small>le 
-                            <?= $data['comment_date_fr']; ?>
-                        </small>
-                    </i>
-                </span><br />
-				
-				<?= nl2br(htmlspecialchars($data['comment'])); ?>
-			</p>
-		</div>
-	
-	<?php
-	}
-	?>
-</body>
-</html>
+    <div class="commentaires">
+        <p>
+            <span class="who">
+                <i>
+                    <strong>
+                        <?= htmlspecialchars($data['author']); ?>
+                    </strong>
+                    <small>le 
+                        <?= $data['comment_date_fr']; ?>
+                    </small>
+                </i>
+            </span><br />
+            
+            <?= nl2br(htmlspecialchars($data['comment'])); ?>
+        </p>
+    </div>
+
+<?php
+}
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
