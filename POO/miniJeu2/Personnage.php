6707ae50a1cfad6c9473ead9731859935ab57e32
Guillaume CARON <gc3d.42@gmail.com>
2018-03-16 13:48:55 +0100

    Modif2 de Personnage

--- POO/miniJeu2/Personnage.php
+++ POO/miniJeu2/Personnage.php
@@ -2,34 +2,61 @@
 class Personnage
 {
     // LES ATTRIBUTS ################################################
-    protected $_id,
-            $_nom,
-            $_degats;
+    protected   $_id,
+                $_nom,
+                $_type,
+                $_degats,
+                $_atout,
+                $timeEndormi;
     
         
     // LES CONSTANTES DE RETOUR D'ACTIONS ##############################
     const CEST_MOI = 1;
     const PERSONNAGE_TUE = 2;
     const PERSONNAGE_FRAPPE = 3;
+    
+    // Constante renvoyée par la méthode `lancerUnSort` (voir classe Magicien) si on a bien ensorcelé un personnage.
+    const PERSONNAGE_ENSORCELE = 4;
+    
+    // Constante renvoyée par la méthode `lancerUnSort` (voir classe Magicien) si on veut jeter un sort alors que la magie du magicien est à 0.
+    const PAS_DE_MAGIE = 5;
+    
+    // Constante renvoyée par la méthode `frapper` si le personnage qui veut frapper est endormi.
+    const PERSO_ENDORMI = 6;
+
 
 
     public function __construct(array $donnees)
     {
         $this->hydrate($donnees);
+        $this->type = strtolower(static::class);
     }
 
 
 
 
     // ACTIONS ################################################
+    public function estEndormi()
+    {
+        return $this->timeEndormi > time();
+    }
+
+
+
+
     public function frapper(Personnage $perso)
     {
         // Avant tout : vérifier qu'on ne se frappe pas soi-même.
         // Si c'est le cas, on stoppe tout en 
         // renvoyant une valeur signifiant que le personnage ciblé est le personnage qui attaque.
-        if ($perso->id() == $this->_id)
+        if ($perso->getId() == $this->_id)
         {
             return self::CEST_MOI;
+        }
+
+        if ($this->estEndormi())
+        {
+            return self::PERSO_ENDORMI;
         }
 
         // On indique au personnage frappé qu'il doit recevoir des dégâts.
@@ -83,12 +110,36 @@ class Personnage
 
 
 
+    public function reveil()
+    {
+        $secondes = $this->timeEndormi;
+        $secondes -= time();
+
+        $heures = floor($secondes / 3600);
+        $secondes -= $heures * 3600;
+        $minutes = floor($secondes / 60);
+        $secondes -= $minutes * 60;
+
+        $heures .= $heures <= 1 ? ' heure' : ' heures';
+        $minutes .= $minutes <= 1 ? ' minute' : ' minutes';
+        $secondes .= $secondes <= 1 ? ' seconde' : ' secondes';
+
+        return $heures . ', ' . $minutes . ' et ' . $secondes;
+    }
+
+
+
+
+    
 
     // Liste des getters ################################################
-    public function id()         { return $this->_id; }
-    public function nom()        { return $this->_nom; }
-    public function degats()     { return $this->_degats; }
-
+    public function getId()            { return $this->id; }
+    public function getNom()           { return $this->nom; }
+    public function getType()         { return $this->type; }
+    public function getDegats()        { return $this->degats; }
+    public function getAtout()         { return $this->atout; }
+    public function getTimeEndormi()   { return $this->timeEndormi; }
+    
 
 
 
@@ -100,7 +151,7 @@ class Personnage
         $id = (int) $id; // Convertit en entier. Souvent '0' si pas entier
         if ($id > 0)
         {
-            $this->_id = $id;
+            $this->id = $id;
         }
     }
 
@@ -108,7 +159,17 @@ class Personnage
     {
         if (is_string($nom))
         {
-            $this->_nom = $nom;
+            $this->nom = $nom;
+        }
+    }
+
+    public function setAtout($atout)
+    {
+        $atout = (int) $atout;
+
+        if ($atout >= 0 && $atout <= 100)
+        {
+            $this->atout = $atout;
         }
     }
 
@@ -117,7 +178,12 @@ class Personnage
         $degats = (int) $degats;
         if ($degats >= 0 && $degats <= 100)
         {
-            $this->_degats = $degats;
+            $this->degats = $degats;
         }
+    }
+
+    public function setTimeEndormi($time)
+    {
+        $this->timeEndormi = (int) $time;
     }
 }
